1f1c20c11b109284c7d6f3535197e1332ae5a818
Kevin Cantrell <kevin903@live.com>
2021-09-06 00:56:14 +0900

    SSR working!

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,29 @@
 import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
+
+const production = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		scss: {
+      includePaths: ['theme', 'src/*'],
+    },
+	}),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			optimizeDeps: {
+				include: ['@carbon/charts'],
+			},
+			ssr: {
+				noExternal: [production && '@carbon/charts'].filter(Boolean),
+			},
+		},
 	}
 };
 
