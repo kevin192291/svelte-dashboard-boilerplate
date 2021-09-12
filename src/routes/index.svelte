085c1b0579b18ad163d2086716f923f61dd607e2
Kevin Cantrell <kevin903@live.com>
2021-09-12 19:07:30 +0900

    working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,19 +4,49 @@
 
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
+	import { addMessages, getLocaleFromNavigator, init, locale, _ } from 'svelte-i18n'
+	import en from './en.json';
+	import jp from './jp.json';
+	import { createEventDispatcher } from 'svelte';
+    export let value = 'en';
+    const dispatch = createEventDispatcher();
+    function handleLocaleChange(event) {
+        event.preventDefault();
+        dispatch('locale-changed', event.target.value);
+		locale.set(event.target.value);
+    }
+
+	addMessages('en', en);
+	addMessages('en-US', en);
+	addMessages('jp', jp);
+
+	init({
+		fallbackLocale: 'en',
+		initialLocale: getLocaleFromNavigator(),
+	});
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Hello</title>
 </svelte:head>
 
 <section>
 	<h1>
 		<div class="welcome">
+			<p>{$_('page_title')}</p>
 			<picture>
 				<source srcset="svelte-welcome.webp" type="image/webp" />
 				<img src="svelte-welcome.png" alt="Welcome" />
 			</picture>
+		</div>
+
+		<div class="locale-selector">
+			<div class="select">
+				<select value={value} on:change={handleLocaleChange}>
+					<option value="en">English</option>
+					<option value="jp">日本語</option>
+				</select>
+			</div>
 		</div>
 
 		to your new<br />SvelteKit app
